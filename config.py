a6a2621f6e93a2dc2fc46efabc3165cadba1c954
johnny <j7609274111@gmail.com>
2021-03-17 12:50:53 -0700

    initial comitt

--- config.py
+++ config.py
@@ -24,16 +24,16 @@ else:
 
 
 # JitBit Information
-HELPDESK_URL = 'http://support.arizonapipeline.com/helpdesk'
-HELPDESK_USER = 'jwhitworth@arizonapipeline.com'
-HELPDESK_PWD = '1Panez00'
+HELPDESK_URL = os.getenv('JITBIT_SUPPORT_URL')
+HELPDESK_USER = os.getenv('JITBIT_USER')
+HELPDESK_PWD = os.getenv('JITBIT_PASSWORD')
 
 # Constants
 CONFIRMS = ["y", "yes", "yep", "yea",]
 DENIES = ["n", "no", "nope",  "nah"]
 
 # Info
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 DESCRIPTION = """
 Wadworth was created specifically for the Arizona Pipeline IT department to better handle communications and to streamline many repetitive tasks.
 """
