f964320d8263554c4d9cb304ca5ff3a33efa1439
Johnny Whitworth <johnny.whitworth@azp.local>
2021-03-31 19:39:08 -0700

    create email notification for keys LTE to 3

--- apps/office/tasks.py
+++ apps/office/tasks.py
@@ -16,13 +16,16 @@ class OfficeTasks(commands.Cog, OfficeTable, name='office tasks'):
         self.check_for_key_count.start()
         self.channel = self.bot.get_channel(core.config.BOT_CHANNEL)
 
-    @tasks.loop(minutes=5.0)
+    @tasks.loop(seconds=5.0)
     async def check_for_key_count(self):
         count = self.count_keys()[0][0]
-        subject = 'Office Keys'
-        message = f'There are {count} available keys remaining'
-        try:
-            send_email(subject, message)
-        except Exception as e:
-            print(e)
+        if count <= 3:
+            subject = 'Office Keys'
+            message = f'There are {count} available keys remaining'
+            try:
+                send_email(subject, message)
+            except Exception as e:
+                print(e)
+        else:
+            print(f'{count} keys remaining')
 
