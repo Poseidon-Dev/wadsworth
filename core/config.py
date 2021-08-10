ed6c2b9ab75eb472d2a96e917402826557a9674c
Johnny Whitworth <johnny.whitworth@azp.local>
2021-08-10 13:20:51 -0700

    update config

--- core/config.py
+++ core/config.py
@@ -52,14 +52,14 @@ CHANNEL = BOT.get_channel(BOT_CHANNEL)
 # CURRENT MODULES
 STARTUP_COGS = [
     'apps.info',
-    # 'apps.directory',
-    # 'apps.office',
-    # 'apps.password',
-    # 'apps.censor',
+    'apps.directory',
+    'apps.office',
+    'apps.password',
+    'apps.censor',
     'apps.erp',
-    # 'apps.support',
-    # 'apps.inventory',
-    # 'apps.emailtest',
+    'apps.support',
+    'apps.inventory',
+    'apps.emailtest',
     # 'apps.scheduler'
     'apps.transfers',
 ] 
