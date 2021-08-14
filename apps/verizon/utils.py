791678968a4d62c9a741de6257a293b355a2fcbf
Johnny Whitworth <johnny.whitworth@azp.local>
2021-08-13 21:13:28 -0700

    split divisions

--- apps/verizon/utils.py
+++ apps/verizon/utils.py
@@ -1,3 +1,4 @@
+from numpy import ALLOW_THREADS
 import pandas as pd 
 import re
 
@@ -24,11 +25,45 @@ def verizon_csv():
     verizon_data_df['Account'] = verizon_data_df['Account'].apply(lambda x: re.match('="(.*)"', x) if x else '')
     verizon_data_df['Account'] = verizon_data_df['Account'].apply(lambda x: x.group(1) if x else '')
 
-    # Test
-    verizon_data_df = pd.to_numeric(verizon_data_df['Total'])
+    # Veriify No Extra Cost Centers
+    divisions = verizon_data_df[['Account']].drop_duplicates()
+    extra_divivions = divisions[~divisions['Account'].isin(ALLOWED_DIVISIONS)]
+
+    print("please correct your data")
+    print(extra_divivions)
+
+    # Seperate dataframes
+    seperate_divivions = {
+        divivions: verizon_data_df[verizon_data_df['Account'].isin([divivions])]
+        for divivions in ALLOWED_DIVISIONS
+    }
+
+    for div, df in seperate_divivions.items():
+        df = df.append(df.sum(numeric_only=True), ignore_index=True)
+        print(df.tail())
+
+
+       
+
+
+ALLOWED_DIVISIONS = [
+    'AVAILABLE',
+    'BULLHEAD',
+    'CARSON CITY',
+    'CORONA',
+    'CORPORATE',
+    'HESPERIA',
+    'LAS VEGAS',
+    'N NEVADA',
+    'PACIFIC',
+    'PHOENIX',
+    'PIPELINE',
+    'SHOP',
+    'TUCSON',
+    ]
+    
 
 
 
     # verizon_data_df['User'] = verizon_data_df['Number'].apply(lambda x: str(x).split(' / ') if x else '')
     # verizon_data_df['Phone'] = verizon_data_df['Number'].apply(lambda x: str(x).split(' / ')[0] if x else '')
-    print(verizon_data_df.head())
